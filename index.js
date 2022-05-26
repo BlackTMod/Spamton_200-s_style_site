7edd71d9764af44710248d71cdf2deaf6695e743
SpamtonTheSpammer <95943416+BlackTMod@users.noreply.github.com>
2022-05-26 01:52:48 -0300

    First release!
    Version 1.0:
    - Just the site

---
+++ index.js
@@ -0,0 +1,22 @@
+function alertButton() {
+  
+alert ("insert pop-up here");
+}
+function AboutSpamton() {
+  
+  window.open ("https://deltarune.fandom.com/wiki/Spamton");
+}
+function ChapterLeak() {
+  window.open ("https://m.youtube.com/watch?v=dQw4w9WgXcQ")
+
+}
+
+function PornPage() {
+  
+  window.open ("https://m.youtube.com/watch?v=dxJyI8Vl46Y")
+}
+function News() {
+
+window.open ("News")
+
+}
